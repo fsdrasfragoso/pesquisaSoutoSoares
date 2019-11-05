c935178cf01cd32e9508b1d505cbb4a3ccc0ed3c
Neto Sales <contato@netosales.com.br>
2019-11-05 20:19:29 -0300

    Pagina inicial

---
+++ formulario_inicio.php
@@ -0,0 +1,30 @@
+<header>
+
+	<div id="search">
+		<img src="logoSoutoSoares.png"/>
+
+		 <!-- <form class="form-signin" role="form"> -->
+			<form action ="quest1.php" method = "post" id="form">
+		        <h2 class="form-signin-heading">Formulario de Pesquisa</h2>
+		        <br><br>
+		        
+		        <button class="btn btn-lg btn-primary " type="submit">Questionario 1 - Familia</button>
+		   	<br><br>
+			</form>
+			<form action="aguarde.php" method = "post" id="form">
+			 <button class="btn btn-lg btn-primary " type="submit">Questionario 2 - Adolescente</button>
+ 
+
+			</form>
+	    <!--  </form> -->
+
+		    <form action ="aguarde.php" method = "post">
+		    	<label>
+					 <h3 class="form-signin-heading">www.netosales.com.br</h3>
+			    </label>
+			    <button class="btn btn-lg btn-primary type="submit">Relatorios</button></a>
+				
+			</form>
+	</div>
+	
+</header>
