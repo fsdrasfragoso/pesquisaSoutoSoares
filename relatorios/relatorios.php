48e47c36ac8133571462e2b30f02bc693ebb0473
fsdrasfragoso <esdrasfragoso@gmail.com>
2019-12-29 15:14:30 -0300

    painel de controle

--- relatorios/relatorios.php
+++ relatorios/relatorios.php
@@ -2,18 +2,22 @@
 
 include('classes/DB.class.php');
 
-$sql = "DESC aluno;"; 
+$sql = "SELECT * FROM relatorios;"; 
 
 $select = DB::getConn()->prepare($sql); 
 $select->execute();
 
 $resgistros = $select->fetchAll();
 
-$campos = []; 
-
 foreach($resgistros as $res){
-    array_push($campos,$res['Field']);
+      echo '<h3>'.$res['nome_relatorio'].'</h3> <br/>';
+      $tes = DB::getConn()->prepare($res['query_relatorio']);
+      $tes->execute(); 
+      $relatorios = $tes->fetchAll();
+      echo '<pre>';
+      print_r($relatorios);
+      echo '</pre>';
 }
-print_r($campos);
+
 
 ?>
